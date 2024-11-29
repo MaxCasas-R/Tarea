0ac1622a22ca00b557ee0ffbb6bc364f5644e968
Max Casas <ezromax18@hotmail.com>
2024-11-28 22:01:27 -0600

    Primera ventana, y botones iniciales incluidos

--- Tarea3IU/application/Main.java
+++ Tarea3IU/application/Main.java
@@ -1,26 +1,47 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+	public static void main (String[]args) {
+		launch();
+	}	
+	public void start (Stage stage) {
+		GridPane gridPane=creaPane();
+		BotonesIniciales(gridPane);
+		//Creamos la escena 
+		Scene scene=new Scene(gridPane);
+		stage.setMaximized(true);//Con esto hacemos que este en pantalla completa
+		stage.setTitle("Prime-Wheels");
+		stage.setScene(scene);
+		stage.show();
 	}
+private void BotonesIniciales(GridPane gridPane) { //Funcion para los botones
+	Button botonVehiculos=new Button("Vehiculos");
+	botonVehiculos.setPrefSize(300, 70);
+	Button botonComprar=new Button("Comprar");
+	botonComprar.setPrefSize(300, 70);
+	Button botonConocenos=new Button("Conocenos");
+	botonConocenos.setPrefSize(300, 70);
+	gridPane.add(botonVehiculos, 0, 0);
+	gridPane.add(botonComprar, 8, 0);
+	gridPane.add(botonConocenos, 17, 0);
 	
-	public static void main(String[] args) {
-		launch(args);
+	}
+private GridPane creaPane() { //Creamos la cuadricula
+	GridPane gridPane=new GridPane();
+	gridPane.setAlignment(Pos.TOP_LEFT);
+	gridPane.setPadding(new Insets(20,20,20,20));
+	gridPane.setVgap(30);
+	gridPane.setHgap(30);
+	return gridPane;
 	}
 }
