fc62e40825ef684bb9d0ffa598d67f8c3461628b
ezrom <ezrom@CasasTUF>
2025-01-04 17:05:20 -0600

    Agregué unas cosas para la conexion con la bd

---
+++ Tarea3IU/application/Conector.java
@@ -0,0 +1,14 @@
+package application;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conector {
+    private static final String URL = "jdbc:mysql://localhost:3306/agencia";
+    private static final String USER = "root";
+    private static final String PASSWORD = "18112003";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
---
+++ Tarea3IU/application/Pruebaconexion.java
@@ -0,0 +1,20 @@
+package application;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Pruebaconexion {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/agencia";
+        String user = "root";
+        String password = "18112003";
+
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            System.out.println("¡Conexión exitosa!");
+        } catch (SQLException e) {
+            System.out.println("Error al conectar a la base de datos:");
+            e.printStackTrace();
+        }
+    }
+
+}
